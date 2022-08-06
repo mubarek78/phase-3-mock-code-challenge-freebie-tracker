7177815bd530bf75ec4a15b225e905a4e2e2152c
mubarek78 <shifamubarek@yahoo.com>
2022-08-06 15:58:00 -0600

    assigment complited

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one? item_name
+        self.freebies.any? {|f| f.item_name == item_name}
+    end
+
+    def give_away dev, freebie
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+            freebie.save
+        else
+            raise Exception.new "Unpermitted to perform this action."
+        end
+    end
 end
---
+++ db/migrate/20220806214817_create_freebies.rb
@@ -0,0 +1,10 @@
+class CreateFreebies < ActiveRecord::Migration[6.1]
+  def change 
+    create_table :freebies do |t|
+      t.string :item_name
+      t.integer :value
+      t.references :dev, foreign_key: true
+      t.references :company, foreign_key: true
+    end
+  end
+end
